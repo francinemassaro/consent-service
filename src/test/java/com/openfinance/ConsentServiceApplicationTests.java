359d8ac9703b1757e96fef4a960eb8a93b82afa5
francinemassaro <francine.massaro@gmail.com>
2025-07-17 19:14:23 -0300

    new repository

---
+++ src/test/java/com/openfinance/ConsentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
